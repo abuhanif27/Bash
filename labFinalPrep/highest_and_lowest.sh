97a13f9ce7a4bb12d3d4c962a841c53d4ddd289d
Abu Hanif <ahanif2500@gmail.com>
2024-12-19 15:41:48 +0600

    implemented highest and lowest item from the array

---
+++ labFinalPrep/highest_and_lowest.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+declare -a array
+read -p "how many itesm you want in your array?: " n
+
+#Enter elements
+for ((i = 0; i < n; i++)); do
+    read -p "Enter value of item ($i): " value
+    array[$i]=$value
+done
+
+echo "Array: ${array[@]}"
+
+#bubble sort
+for ((i = 0; i < n; i++)); do
+    for ((j = 1; j < n - i; j++)); do
+        if ((${array[$j]} > ${array[$j - 1]})); then
+            temp=${array[$j - 1]}
+            array[$j - 1]=${array[$j]}
+            array[$j]=$temp
+        fi
+    done
+done
+
+highest=${array[0]}
+lowest=${array[n - 1]}
+
+echo "Sorted Array: ${array[@]}"
+
+echo "Highest item from the array: $highest"
+
+echo "Lowest item from the array: $lowest"
